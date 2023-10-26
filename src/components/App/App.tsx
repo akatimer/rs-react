84e7fb8995eb3b3d67e7d5a5bccc7dd8d7c769e6
akatimer <akatimer@gmail.com>
2023-10-26 16:05:13 +0300

    feat: add TopSection.tsx & changed folder strctr

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './App.css';
+import TopSection from '../TopSection/TopSection';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello</h1>
+        <TopSection />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TopSection/TopSection.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import './TopSection.css';
+
+class TopSection extends Component {
+  render() {
+    return <h2>TopSection</h2>;
+  }
+}
+
+export default TopSection;
