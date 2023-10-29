9f12411cac876243926f223bff4eda4049e86d17
akatimer <akatimer@gmail.com>
2023-10-29 13:04:58 +0200

    feat: add card with state & props

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './Catalog.css';
 import DataContext from '../Context/DataContext';
+import Card from '../Card/Card';
 
 class Catalog extends Component {
   declare context: React.ContextType<typeof DataContext>;
@@ -10,7 +11,21 @@ class Catalog extends Component {
     return (
       <div>
         {cards.map((card) => (
-          <div key={card.url}>{card.name}</div>
+          <div key={card.url}>
+            <Card
+              name={card.name}
+              model={card.model}
+              starship_class={card.starship_class}
+              cost_in_credits={card.cost_in_credits}
+              length={card.length}
+              crew={card.crew}
+              passengers={card.passengers}
+              max_atmosphering_speed={card.max_atmosphering_speed}
+              hyperdrive_rating={card.hyperdrive_rating}
+              MGLT={card.MGLT}
+              cargo_capacity={card.cargo_capacity}
+            />
+          </div>
         ))}
       </div>
     );
