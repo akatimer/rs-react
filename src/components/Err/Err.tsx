549138e55c377790c653c49b986076431589b21b
akatimer <akatimer@gmail.com>
2023-10-29 16:08:10 +0200

    feat: add errorBoundary & Err component

---
+++ src/components/Err/Err.tsx
@@ -0,0 +1,19 @@
+// function Err() {
+//   throw new Error('Error!!!');
+//   return <div></div>;
+// }
+
+import { Component } from 'react';
+
+class Err extends Component {
+  errFunc() {
+    throw new Error('Error!!!');
+    return <div></div>;
+  }
+
+  render() {
+    return <div>{this.errFunc()}</div>;
+  }
+}
+
+export default Err;
--- src/components/TopSection/TopSection.tsx
+++ src/components/TopSection/TopSection.tsx
@@ -2,11 +2,27 @@ import { Component } from 'react';
 import './TopSection.css';
 import SearchPanel from '../SearchPanel/SearchPanel';
 import DataContext from '../Context/DataContext';
+import Err from '../Err/Err';
 
-class TopSection extends Component {
+class TopSection extends Component<Record<string, never>, { newErr: boolean }> {
   declare context: React.ContextType<typeof DataContext>;
+  state = {
+    newErr: false,
+  };
   render() {
-    return <SearchPanel state={{ value: this.context.searchValue }} />;
+    return (
+      <>
+        <button
+          onClick={() => {
+            this.setState({ newErr: true });
+          }}
+        >
+          Error
+        </button>
+        {this.state.newErr && <Err />}
+        <SearchPanel state={{ value: this.context.searchValue }} />
+      </>
+    );
   }
 }
 
