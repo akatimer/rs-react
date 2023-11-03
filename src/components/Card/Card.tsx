7a1e457686d20182676d231f10a6ccdbe92633db
akatimer <akatimer@gmail.com>
2023-11-03 20:18:18 +0200

    refactor: migrated Card to function

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React from 'react';
 import './Card.css';
 
 export type cardStateType = {
@@ -22,70 +22,51 @@ export type cardStateType = {
   edited?: string;
 };
 
-class Card extends Component<cardStateType, cardStateType> {
-  constructor(props: cardStateType) {
-    super(props);
-    this.state = {
-      name: this.props.name,
-      model: this.props.model,
-      starship_class: this.props.starship_class,
-      cost_in_credits: this.props.cost_in_credits,
-      length: this.props.length,
-      crew: this.props.crew,
-      passengers: this.props.passengers,
-      max_atmosphering_speed: this.props.max_atmosphering_speed,
-      hyperdrive_rating: this.props.hyperdrive_rating,
-      MGLT: this.props.MGLT,
-      cargo_capacity: this.props.cargo_capacity,
-    };
-  }
-
-  render() {
-    return (
-      <div className="card">
-        <div className="card__line">
-          <h3>Name: </h3>
-          <h3>{this.state.name}</h3>
-        </div>
-        <div className="card__line">
-          <h3>Model: </h3>
-          <h3>{this.state.model}</h3>
-        </div>
-        <div className="card__line">
-          <h3>Starship_class: </h3>
-          <h3>{this.state.starship_class}</h3>
-        </div>
-        <div className="card__line">
-          <h3>Cost_in_credits: </h3>
-          <h3>{this.state.cost_in_credits}</h3>
-        </div>
-        <div className="card__line">
-          <h3>Length: </h3>
-          <h3>{this.state.length}</h3>
-        </div>
-        <div className="card__line">
-          <h3>Crew: </h3>
-          <h3>{this.state.crew}</h3>
-        </div>
-        <div className="card__line">
-          <h3>Passengers: </h3>
-          <h3>{this.state.passengers}</h3>
-        </div>
-        <div className="card__line">
-          <h3>Max_atm_speed: </h3>
-          <h3>{this.state.max_atmosphering_speed}</h3>
-        </div>
-        <div className="card__line">
-          <h3>MGLT: </h3>
-          <h3>{this.state.MGLT}</h3>
-        </div>
-        <div className="card__line">
-          <h3>Cargo_capacity: </h3>
-          <h3>{this.state.cargo_capacity}</h3>
-        </div>
+const Card: React.FC<cardStateType> = (props): JSX.Element => {
+  return (
+    <div className="card">
+      <div className="card__line">
+        <h3>Name: </h3>
+        <h3>{props.name}</h3>
       </div>
-    );
-  }
-}
+      <div className="card__line">
+        <h3>Model: </h3>
+        <h3>{props.model}</h3>
+      </div>
+      <div className="card__line">
+        <h3>Starship_class: </h3>
+        <h3>{props.starship_class}</h3>
+      </div>
+      <div className="card__line">
+        <h3>Cost_in_credits: </h3>
+        <h3>{props.cost_in_credits}</h3>
+      </div>
+      <div className="card__line">
+        <h3>Length: </h3>
+        <h3>{props.length}</h3>
+      </div>
+      <div className="card__line">
+        <h3>Crew: </h3>
+        <h3>{props.crew}</h3>
+      </div>
+      <div className="card__line">
+        <h3>Passengers: </h3>
+        <h3>{props.passengers}</h3>
+      </div>
+      <div className="card__line">
+        <h3>Max_atm_speed: </h3>
+        <h3>{props.max_atmosphering_speed}</h3>
+      </div>
+      <div className="card__line">
+        <h3>MGLT: </h3>
+        <h3>{props.MGLT}</h3>
+      </div>
+      <div className="card__line">
+        <h3>Cargo_capacity: </h3>
+        <h3>{props.cargo_capacity}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
