7f168f91be0cf5aef8275c92bef0473b17aa79e1
akatimer <akatimer@gmail.com>
2023-11-03 18:54:28 +0200

    refactor: migrated App component to function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
 import './App.css';
 import TopSection from '../TopSection/TopSection';
 import MiddleSection from '../MiddleSection/MiddleSection';
 import { DataProvider } from '../Context/DataContext';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1>Starship Searcher</h1>
-        <DataProvider>
-          <TopSection />
-          <MiddleSection />
-        </DataProvider>
-      </>
-    );
-  }
-}
+const App: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <h1>Starship Searcher</h1>
+      <DataProvider>
+        <TopSection />
+        <MiddleSection />
+      </DataProvider>
+    </>
+  );
+};
 
 export default App;
