a2a9ceb554fae638c4ffb9d1967af35656a0d9f1
akatimer <akatimer@gmail.com>
2023-11-05 21:48:04 +0200

    feat: fixed loader on detailed card

--- src/components/CardDetailed/CardDetailed.tsx
+++ src/components/CardDetailed/CardDetailed.tsx
@@ -23,61 +23,63 @@ const CardDetailed: React.FC = (): JSX.Element => {
   const navigation = useNavigate();
 
   return (
-    <Suspense fallback={<Loader />}>
-      <Await resolve={data}>
-        {(data) => {
-          return (
-            <>
-              <div className="card__detailed">
-                <Link
-                  className="link_button"
-                  to={
-                    term
-                      ? `/limit/${limit}/page/${page}/term/${term}`
-                      : `/limit/${limit}/page/${page}/term/`
-                  }
-                >
-                  {'CLOSE'}
-                </Link>
-                <div className="card__detailed__img">
-                  <img src={data.data.images.webp.image_url} />
-                </div>
-                <div className="card__info">
-                  <div className="card__line">
-                    <h3>Title: </h3>
-                    <h3>{data.data.title}</h3>
-                  </div>
-                  <div className="card__line">
-                    <h3>Score: </h3>
-                    <h3>{data.data.score || '---'}</h3>
-                  </div>
-                  <div className="card__line">
-                    <h3>Status: </h3>
-                    <h3>{data.data.status}</h3>
-                  </div>
-                  <div className="card__line__synopsys">
-                    <h3>Synopsys: </h3>
-                    <h3>{data.data.synopsis || '---'}</h3>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="close_wrapper"
-                onClick={() =>
-                  navigation(
-                    `${
+    <div className="card__detailed">
+      <Suspense fallback={<Loader />}>
+        <Await resolve={data}>
+          {(data) => {
+            return (
+              <>
+                <div className="card__detailed">
+                  <Link
+                    className="link_button"
+                    to={
                       term
                         ? `/limit/${limit}/page/${page}/term/${term}`
                         : `/limit/${limit}/page/${page}/term/`
-                    }`
-                  )
-                }
-              ></div>
-            </>
-          );
-        }}
-      </Await>
-    </Suspense>
+                    }
+                  >
+                    {'CLOSE'}
+                  </Link>
+                  <div className="card__detailed__img">
+                    <img src={data.data.images.webp.image_url} />
+                  </div>
+                  <div className="card__info">
+                    <div className="card__line">
+                      <h3>Title: </h3>
+                      <h3>{data.data.title}</h3>
+                    </div>
+                    <div className="card__line">
+                      <h3>Score: </h3>
+                      <h3>{data.data.score || '---'}</h3>
+                    </div>
+                    <div className="card__line">
+                      <h3>Status: </h3>
+                      <h3>{data.data.status}</h3>
+                    </div>
+                    <div className="card__line__synopsys">
+                      <h3>Synopsys: </h3>
+                      <h3>{data.data.synopsis || '---'}</h3>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="close_wrapper"
+                  onClick={() =>
+                    navigation(
+                      `${
+                        term
+                          ? `/limit/${limit}/page/${page}/term/${term}`
+                          : `/limit/${limit}/page/${page}/term/`
+                      }`
+                    )
+                  }
+                ></div>
+              </>
+            );
+          }}
+        </Await>
+      </Suspense>
+    </div>
   );
 };
 
