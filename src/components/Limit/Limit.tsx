ccd8f31bc9c0c7f6e769d4b3d53c92873ea25974
akatimer <akatimer@gmail.com>
2023-11-19 14:23:11 +0200

    feat: add sliceLimit

--- src/components/Limit/Limit.tsx
+++ src/components/Limit/Limit.tsx
@@ -1,14 +1,19 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import './Limit.css';
+import sliceLimit from '../../store/sliceLimit';
+import { useAppDispatch } from '../../store/hooks';
 
 const Limit: React.FC = (): JSX.Element => {
   const navigate = useNavigate();
   const { page } = useParams();
+  const dispatch = useAppDispatch();
+
   return (
     <div className="limits">
       <button
         className="limits__button"
         onClick={() => {
+          dispatch(sliceLimit.actions.setSliceLimit('12'));
           navigate(
             `/limit/${12}/page/1/term/${
               localStorage.getItem('mangaSearch') || ''
@@ -22,6 +27,7 @@ const Limit: React.FC = (): JSX.Element => {
       <button
         className="limits__button"
         onClick={() => {
+          dispatch(sliceLimit.actions.setSliceLimit('15'));
           navigate(
             `/limit/${15}/page/1/term/${localStorage.getItem('mangaSearch')}`
           );
@@ -32,6 +38,7 @@ const Limit: React.FC = (): JSX.Element => {
       <button
         className="limits__button"
         onClick={() => {
+          dispatch(sliceLimit.actions.setSliceLimit('18'));
           navigate(
             `/limit/${18}/page/1/term/${localStorage.getItem('mangaSearch')}`
           );
@@ -42,6 +49,7 @@ const Limit: React.FC = (): JSX.Element => {
       <button
         className="limits__button"
         onClick={() => {
+          dispatch(sliceLimit.actions.setSliceLimit('21'));
           navigate(
             `/limit/${21}/page/1/term/${localStorage.getItem('mangaSearch')}`
           );
