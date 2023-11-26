347512829d7a6544782287a4752fe2e762d604f7
akatimer <akatimer@gmail.com>
2023-11-26 20:29:01 +0200

    refactor: cleared empty line

--- src/app/@modal/[detailed]/page.tsx
+++ src/app/@modal/[detailed]/page.tsx
@@ -23,7 +23,6 @@ export default async function Detailed(params: {
     <div>
       <ModalWindow>
         <Suspense fallback={<Loader />}>
-
           <CardDetailed data={data} />
         </Suspense>
       </ModalWindow>
