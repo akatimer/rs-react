9de26da5e38533e0f84734d954836cfe7135ce8b
akatimer <akatimer@gmail.com>
2023-12-03 22:11:18 +0200

    feat: add uncontroled cards layout

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,29 @@
+import { CardsType } from '../../store/slice/formDataCardsSlice';
+import './card.css';
+
+type CardProps = {
+  card: CardsType;
+  image: string;
+  lastUpdate: string;
+};
+
+const Card: React.FC<CardProps> = ({
+  card,
+  image,
+  lastUpdate,
+}): JSX.Element => {
+  console.log(card, image, lastUpdate, 123123123);
+  return (
+    <div style={lastUpdate ? { border: '1px solid orange' } : {}}>
+      <p>Name: {card.name}</p>
+      <p>Age: {card.age}</p>
+      <p>Email: {card.email}</p>
+      <p>Gender: {card.gender}</p>
+      <div>
+        <img src={image} alt={card.name} className="card-image" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
